634d50a0d59832f9585607649ee25144b9171fe1
CT2B145 <mjpon45@gmail.com>
2016-08-30 08:27:54 -0700

    TestCode
    A in class proyecto that we did.

---
+++ TestJar/src/TestCode.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mitchell Pon
+ * Date: Aug 30, 2016
+ *TestCode program -- used to show how to create a jar file that i can see.
+ */
+public class TestCode {
+
+	/** Welcome!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
